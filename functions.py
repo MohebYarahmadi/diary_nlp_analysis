e0ab24166b216b7e9eafbff89040171a2208620e
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-04-29 12:36:31 +0330

    Done

--- functions.py
+++ functions.py
@@ -1,6 +1,5 @@
-import re, nltk
-from nltk.corpus import stopwords
 from nltk.sentiment import SentimentIntensityAnalyzer
+from pathlib import Path
 
 # nltk.download('vader_lexicon')
 # en_st_words = stopwords.words("english")
@@ -10,15 +9,18 @@ def read_files(path):
         return file.read()
 
 
-def word_list(data):
-    pattern = re.compile("[a-zA-z]+")
-    return re.findall(pattern, data)
-
-
 def sentiment_analyzer(data):
     analyzer = SentimentIntensityAnalyzer()
     score = analyzer.polarity_scores(data)
-    return score
+    return score    # output: {'neg': 0.03, 'neu': 0.8, 'pos': 0.17, 'compound': 0.966}
+
+
+
+def get_dates(filepath):
+    dates = [Path(f).stem for f in filepath]
+    dates.sort()
+    return dates
+
 
 if __name__ == "__main__":
     print("file read and analyse here")
--- main.py
+++ main.py
@@ -4,15 +4,25 @@ import glob
 import functions
 
 filepaths = glob.glob("data/*.txt")
+dates = functions.get_dates(filepaths)
+pos_scores = []
+neg_scores = []
 
-st.set_page_config(page_title='Diary Analysis', layout='wide')
+st.set_page_config(page_title='Diary Tone')
 st.title('Diary Analysis using NLP')
-st.subheader('You can see the diagrams here')
+st.subheader('Diary Tone')
 
 for filepath in filepaths:
     diary = functions.read_files(filepath)
     scores = functions.sentiment_analyzer(diary)
-    print(filepath)
-    print(scores)
-    # figure = px.line(x=dates, y=pos, labels={'x': 'date', 'y': 'moode'})
-    # st.plotly_chart(figure)
+    pos_scores.append(scores['pos'])
+    neg_scores.append(scores['neg'])
+
+
+st.subheader("Positivity")
+pos_figure = px.line(x=dates, y=pos_scores, labels={'x': 'Dates', 'y': 'Positivity'})
+st.plotly_chart(pos_figure)
+
+st.subheader("Negativity")
+neg_figure = px.line(x=dates, y=neg_scores, labels={'x': 'Dates', 'y': 'Negativity'})
+st.plotly_chart(neg_figure)
